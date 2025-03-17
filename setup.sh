c53872326283a3d18140607a60916b16ef9937d3
SunfiboAIDev <sunfiboaidev1@sunfibo.com>
2025-03-17 22:56:51 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -104,7 +104,7 @@ case $PLATFORM in
 esac
 
 if [ "$BASIC" = true ] ; then
-    pip install pillow imageio imageio-ffmpeg tqdm easydict opencv-python-headless scipy ninja rembg onnxruntime trimesh xatlas pyvista pymeshfix igraph transformers
+    pip install pillow imageio imageio-ffmpeg tqdm easydict opencv-python-headless scipy ninja rembg onnxruntime trimesh xatlas pyvista pymeshfix igraph transformers fastapi uvicorn
     pip install git+https://github.com/EasternJournalist/utils3d.git@9a4eb15e4021b67b12c460c7057d642626897ec8
 fi
 
